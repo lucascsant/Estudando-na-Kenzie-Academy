ad8386a4069bb40b69d5a24bb09fca04a66ffdff
lucassant.os <lucao48@gmail.com>
2025-08-23 16:33:51 -0300

    Começando com JavaScript

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Estou no html, arquivo de script interno")
+console.log("Eu funciono")
+
+let meuNumero = 100;
+let outroNumero = 35;
+
+console.log(meuNumero);
+
+meuNumero = 50;
+
+console.log(meuNumero + outroNumero);
+
+console.log(`Meu número é : ${meuNumero + outroNumero}`);
+
+let cursoDeProgramacao = "kenzie academy";
+
+console.log (cursoDeProgramacao);
